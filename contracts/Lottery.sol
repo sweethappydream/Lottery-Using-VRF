b82473a48cb6ff538cb90fba954c0dae4bdc236f
SweetDreamEthan <ethanmartin.0508@gmail.com>
2022-03-31 18:28:27 -0400

    Added Lottery contract and it's deployment script

---
+++ contracts/Lottery.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+
+contract Lottery is VRFConsumerBase, Ownable {
+    address payable[] public players;
+    address payable public recentWinner;
+    uint256 public randomness;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lotteryState;
+    uint256 public fee;
+    bytes32 public keyhash;
+
+    constructor(
+        address _priceFeedAddress,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyhash
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
+        usdEntryFee = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lotteryState = LOTTERY_STATE.CLOSED;
+        fee = _fee;
+        keyhash = _keyhash;
+    }
+
+    function enter() public payable {
+        // $50 Minimum
+        require(lotteryState == LOTTERY_STATE.OPEN, "Lottery is Closed!");
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData(); //Given with 8 decimals
+        uint256 adjustedPrice = uint256(price) * 10**10; //Convert to 18 decimals
+        //$50 in ETH?
+        uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
+        return costToEnter;
+    }
+
+    function startLottery() public onlyOwner {
+        require(
+            lotteryState == LOTTERY_STATE.CLOSED,
+            "Lottery is already open!"
+        );
+        lotteryState = LOTTERY_STATE.OPEN;
+    }
+
+    function endLottery() public onlyOwner {
+        lotteryState == LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requestId = requestRandomness(keyhash, fee);
+    }
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        require(
+            lotteryState == LOTTERY_STATE.CALCULATING_WINNER,
+            "You aren't there yet"
+        );
+        require(_randomness > 0, "Random not found");
+        uint256 indexOfWinner = _randomness % players.length;
+        recentWinner = players[indexOfWinner];
+        recentWinner.transfer(address(this).balance);
+        // Reset
+        players = new address payable[](0);
+        lotteryState = LOTTERY_STATE.CLOSED;
+        randomness = _randomness;
+    }
+}
---
+++ scripts/deploy_lottery.py
@@ -0,0 +1,54 @@
+from typing import NewType
+from scripts.helpful_scripts import get_account, get_contract, fund_with_link
+from brownie import Lottery, accounts, config, network
+import time
+
+
+def deploy_lottery():
+    account = get_account()
+    lotttery = Lottery.deploy(
+        get_contract("eth_usd_price_feed").address,
+        get_contract("vrf_coordinator").address,
+        get_contract("link_token").address,
+        config["networks"][network.show_active()]["fee"],
+        config["networks"][network.show_active()]["keyhash"],
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False),
+    )
+    print("Deployed lotttery!")
+    return lotttery
+
+
+def start_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    starting_tx = lottery.startLottery({"from": account})
+    starting_tx.wait(1)
+    print("The lottery has started")
+
+
+def enter_the_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    value = lottery.getEntranceFee() + 100000000
+    tx = lottery.enter({"from": account, "value": value})
+    print("You entered the lottery!")
+
+
+def end_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    # fund contract with LINK then end
+    tx = fund_with_link(lottery.address)
+    tx.wait(1)
+    ending_tx = lottery.endLottery({"from": account})
+    ending_tx.wait(1)
+    time.sleep(3)
+    print(f"{lottery.recentWinner()} is the new winner!")
+
+
+def main():
+    deploy_lottery()
+    start_lottery()
+    enter_the_lottery()
+    end_lottery()
