e718fa5929e7cbf587bd53625857f86b69a65a21
SweetDreamEthan <ethanmartin.0508@gmail.com>
2022-03-31 20:04:38 -0400

    Added unit and integration testing

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -2,9 +2,14 @@
 from brownie import Lottery, accounts, config, network, exceptions
 import pytest
 from scripts.deploy_lottery import deploy_lottery
+from scripts.helpful_scripts import get_contract
 from web3 import Web3
 
-from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    fund_with_link,
+    get_account,
+)
 
 
 def test_get_entrance_fee():
@@ -35,4 +40,47 @@ def test_can_start_and_enter_lottery():
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip()
     lottery = deploy_lottery()
+    account = get_account()
     # Act
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": get_account(), "value": lottery.getEntranceFee()})
+    # Assert
+    assert lottery.players(0) == account
+
+
+def test_can_end_lottery():
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    fund_with_link(lottery)
+    lottery.endLottery({"from": account})
+    assert lottery.lottery_state() == 2
+
+
+def test_can_pick_winner_correctly():
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    lottery.enter({"from": get_account(index=1), "value": lottery.getEntranceFee()})
+    lottery.enter({"from": get_account(index=2), "value": lottery.getEntranceFee()})
+    fund_with_link(lottery)
+    starting_balance_of_account = account.balance()
+    balance_of_lottery = lottery.balance()
+    transaction = lottery.endLottery({"from": account})
+    request_id = transaction.events["RequestedRandomness"]["requestId"]
+    STATIC_RNG = 777
+    get_contract("vrf_coordinator").callBackWithRandomness(
+        request_id, STATIC_RNG, lottery.address, {"from": account}
+    )
+    # 777 % 3 = 0
+    assert lottery.recentWinner() == account
+    assert lottery.balance() == 0
+    assert account.balance() == starting_balance_of_account + balance_of_lottery
